878d872ded747e097332967b152579f8cb9dd3de
Bryan Gonzales <hello@brnkmarketing.com>
2024-01-04 14:18:57 -0500

    Bryans Website Portfolio (First Draft)

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,81 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "About Me",
+      content: (
+        <>
+          <p>
+          I blend software development and digital marketing to enhance online
+          presence. As a React developer focused on AI, I create custom solutions
+          and run effective campaigns on platforms like Facebook and TikTok for brand growth.
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "2024 Masters Degree",
+      content: (
+        <>
+          <p>
+          I'm pursuing a Master's in Digital Marketing to stay on the forefront
+          of evolving strategies and techniques, enhancing my ability to create
+          innovative online solutions.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2021 Bachelors Degree",
+      content: (
+        <>
+          <p>
+          "I hold a Bachelor's in Computer Science from Georgia State University,
+          equipping me with a solid foundation in technology to complement my
+          expertise in digital marketing.
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "2023 Contract Software Developer",
+      content: (
+        <>
+          <p>
+          As a front-end developer, my focus lies in React, where I leverage
+          my skills to create dynamic and user-friendly web interfaces.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2022 Software Engineer at FIS Global",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2021 Software Engineer at TATA Consultancy Services",
+      content: (
+        <>
+          <p>
+          As a full-stack development intern at Tata Consultancy Services,
+          I contributed to various projects, gaining hands-on experience
+          in web development and enhancing my skills in both
+          front-end and back-end tasks."
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
