431f20609ea390b015b4d8a102c4bfa4629aafdf
Bryan Gonzales <hello@brnkmarketing.com>
2024-01-09 20:29:45 -0500

    about page default

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Nav from "./nav/Nav.js";
+import { Redirect } from "react-router-dom";
 import About from "./about/About";
 import Skills from "./skills/Skills";
 import Projects from "./projects/Projects";
@@ -15,8 +16,8 @@ const App = () => {
       <Nav />
       <Background />
       <Routes>
-        <Route path="/" element={<Navigate to="/about" />} />
-        <Route path="/about" element={<About />} />
+      <Route path="https://bgonzales1.github.io/react-portfolio-v2-main-2/" element={<Navigate to="/" />} />
+        <Route path="/" element={<About/>} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
