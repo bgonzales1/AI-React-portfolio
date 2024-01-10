d9f62510c984891b4fef724f37ec8e6f26766055
Bryan Gonzales <hello@brnkmarketing.com>
2024-01-10 14:01:51 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Nav from "./nav/Nav.js";
 import About from "./about/About";
 import Skills from "./skills/Skills";
@@ -18,19 +18,21 @@ const App = () => {
 
   return (
     <>
-      <Nav />
-      {/* Conditionally render the About component */}
-      {renderAbout && <About />}
-      <Background />
-      <Routes>
-        {/* Redirect the root path to "/about" */}
-        <Route path="/" element={<Navigate to="/about" />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <PlayerStats />
+      <Router>
+        <Nav />
+        {/* Conditionally render the About component */}
+        {renderAbout && <About />}
+        <Background />
+        <Routes>
+          {/* Redirect the root path to "/about" */}
+          <Route path="/" element={<Navigate to="/about" />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <PlayerStats />
+      </Router>
     </>
   );
 };
